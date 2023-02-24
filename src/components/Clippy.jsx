a1a5380cabfe816f0af3f980f716e56dd09f392a
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2023-02-24 15:06:25 +0530

    Update Clippy.jsx

--- src/components/Clippy.jsx
+++ src/components/Clippy.jsx
@@ -4,6 +4,7 @@ function Clippy() {
   return (
     <>
         <div className="clippy-box">
+        <h1>hello</h1>
         <img src="./images/clippy-hi.gif" className="clippy" alt="hey there"></img>
         </div>
     </>
