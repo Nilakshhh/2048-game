20c80c02d978567380db6a7f3e3b973ab04f63f6
Nilakshhh <dnilaksh05@gmail.com>
2023-03-10 15:17:46 +0530

    perfected left and right click function

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -41,8 +41,8 @@ useEffect(() => {
 },[initialArray]);
 
 
-function genRandom(){
-const randomNumber = Math.floor(Math.random() * 4);
+function genRandom(n){
+const randomNumber = Math.floor(Math.random() * n);
 return randomNumber;
 }
 
@@ -54,8 +54,8 @@ function start(){
         newArray[i][j].val=0;
       }
     }
-    var ran1 = genRandom();
-    var ran2 = genRandom();
+    var ran1 = genRandom(newArray.length);
+    var ran2 = genRandom(newArray.length);
     newArray[ran1][ran2].val=2;
     //newArray[0][1].val=2;
         
@@ -67,47 +67,51 @@ function start(){
 
 function handleLeftClick(){
   const newArray = [...initialArray];
-  for(let i = 0; i<newArray.length; i++){
-    for(let j = newArray.length-1; j>0; j--){
-      if(newArray[i][j].val !== 0){
-          if(newArray[i][j-1].val === 0){
-            newArray[i][j-1].val = newArray[i][j].val;
-            newArray[i][j].val = 0;
-          }
-          else if(newArray[i][j-1].val === newArray[i][j].val){
-            newArray[i][j-1].val *= 2;
-            newArray[i][j].val = 0; 
-          }
-          /*else if(newArray[i][j-1].val !== newArray[i][j].val){}*/
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = 1; j < newArray.length; j++) {
+      if (newArray[i][j].val !== 0) {
+        let k = j;
+        while (k > 0 && newArray[i][k-1].val === 0) {
+          newArray[i][k-1].val = newArray[i][k].val;
+          newArray[i][k].val = 0;
+          k--;
+        }
+        if (k > 0 && newArray[i][k-1].val === newArray[i][k].val) {
+          newArray[i][k-1].val *= 2;
+          newArray[i][k].val = 0;
+        }
       }
     }
   }
-  
+
   let checkArray = [];
   for(let i = 0; i < newArray.length; i++){
     if(newArray[i][newArray.length-1].val === 0){
       checkArray.push(i);
     }
   }
-  var ran1 = Math.floor(Math.random() * checkArray.length);
-  newArray[ran1][newArray.length-1].val=2;
-  //newArray[2][newArray.length-1].val=4;  
+  var ran1 = genRandom(checkArray.length);
+  var prob = genRandom(100);
+  newArray[checkArray[ran1]][newArray.length-1].val = prob<93 ? 2 : 4;
   setinitialArray(newArray);
 }
 function handleRightClick(){
   const newArray = [...initialArray];
-  for(let i = 0; i<newArray.length; i++){
-    for(let j = 0; j<newArray[i].length - 1; j++){
-      if(newArray[i][j].val !== 0){
-          if(newArray[i][j+1].val === 0){
-            newArray[i][j+1].val = newArray[i][j].val;
-            newArray[i][j].val = 0;
-          }
-          else if(newArray[i][j+1].val === newArray[i][j].val){
-            newArray[i][j+1].val *= 2;
-            newArray[i][j].val = 0; 
-          }
-          /*else if(newArray[i][j-1].val !== newArray[i][j].val){}*/
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = newArray.length - 2; j >= 0; j--) {
+      if (newArray[i][j].val !== 0) {
+        let k = j;
+        while (k < newArray.length - 1 && newArray[i][k+1].val === 0) {
+          // Shift the tile to the right until it hits the edge or a non-empty tile
+          newArray[i][k+1].val = newArray[i][k].val;
+          newArray[i][k].val = 0;
+          k++;
+        }
+        if (k < newArray.length - 1 && newArray[i][k+1].val === newArray[i][k].val) {
+          // Merge the tile with the adjacent tile if they have the same value
+          newArray[i][k+1].val *= 2;
+          newArray[i][k].val = 0;
+        }
       }
     }
   }
@@ -116,11 +120,12 @@ function handleRightClick(){
   for(let i = 0; i < newArray.length; i++){
     if(newArray[i][0].val === 0){
       checkArray.push(i);
+      console.log("yes");
     }
   }
-  var ran1 = Math.floor(Math.random() * checkArray.length);
-  newArray[ran1][0].val=2;
-  //newArray[2][newArray.length-1].val=4;  
+  var ran1 = genRandom(checkArray.length);
+  var prob = genRandom(100);
+  newArray[checkArray[ran1]][0].val = prob<93 ? 2 : 4;
   setinitialArray(newArray);
 }
 
